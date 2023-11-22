0406e105aa1b05ce6a5ebb63b9e5905afca9a26a
Yarukon <a2692866561@live.com>
2023-11-22 14:42:19 +0800

    shorten rtv time to 10 minutes

--- src/cs2fixes.cpp
+++ src/cs2fixes.cpp
@@ -401,7 +401,7 @@ void CS2Fixes::Hook_StartupServer(const GameSessionConfiguration_t& config, ISou
 	g_ExtendState = EExtendState::MAP_START;
 
 	// Allow RTV and Extend votes after 20 minutes post map start
-	new CTimer(1200.0f, false, []()
+	new CTimer(600.0f, false, []()
 	{
 		if (g_RTVState != ERTVState::BLOCKED_BY_ADMIN)
 			g_RTVState = ERTVState::RTV_ALLOWED;
