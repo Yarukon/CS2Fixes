f01c472e40e89e4680f54c7e0a2295ef2a556d17
Yarukon <a2692866561@live.com>
2024-01-14 02:08:16 +0800

    block sounds

--- src/detours.cpp
+++ src/detours.cpp
@@ -204,10 +204,16 @@ void FASTCALL Detour_CCSWeaponBase_Spawn(CBaseEntity *pThis, void *a2)
 	CCSWeaponBase_Spawn(pThis, a2);
 }
 
+const char* snd1 = "Flesh.BulletImpact";
+const char* snd2 = "Player.DamageHeadShot.Onlooker";
+const char* snd3 = "Player.DamageHeadShot.Victim";
+const char* snd4 = "Player.DamageHeadShotArmor.Victim";
+const char* snd5 = "BaseGrenade.Explode";
 void FASTCALL Detour_CSoundEmitterSystem_EmitSound(ISoundEmitterSystemBase *pSoundEmitterSystem, CEntityIndex *a2, IRecipientFilter &filter, uint32 a4, EmitSound_t* a5)
 {
 	//ConMsg("Detour_CSoundEmitterSystem_EmitSound\n");
-	if (strcmp(a5->m_pSoundName, "Flesh.BulletImpact") == 0)
+	const char* sndName = a5->m_pSoundName;
+	if (strcmp(sndName, snd1) == 0 || strcmp(sndName, snd2) == 0 || strcmp(sndName, snd3) == 0 || strcmp(sndName, snd4) == 0 || strcmp(sndName, snd5) == 0)
 		return;
 
 	CSoundEmitterSystem_EmitSound(pSoundEmitterSystem, a2, filter, a4, a5);
