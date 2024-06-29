305acebb282566fca501708bb0e0fd03e36e0ff2
Chen Buyi <chenbuyi2019@outlook.com>
2024-06-29 16:50:26 +0800

    去除 maxspeed

--- src/detours.cpp
+++ src/detours.cpp
@@ -77,7 +77,7 @@ DECLARE_DETOUR(CGamePlayerEquip_InputTriggerForAllPlayers, Detour_CGamePlayerEqu
 DECLARE_DETOUR(CGamePlayerEquip_InputTriggerForActivatedPlayer, Detour_CGamePlayerEquip_InputTriggerForActivatedPlayer);
 DECLARE_DETOUR(CCSGameRules_GoToIntermission, Detour_CCSGameRules_GoToIntermission);
 DECLARE_DETOUR(GetFreeClient, Detour_GetFreeClient);
-DECLARE_DETOUR(CCSPlayerPawn_GetMaxSpeed, Detour_CCSPlayerPawn_GetMaxSpeed);
+// DECLARE_DETOUR(CCSPlayerPawn_GetMaxSpeed, Detour_CCSPlayerPawn_GetMaxSpeed);
 
 void FASTCALL Detour_CGameRules_Constructor(CGameRules* pThis)
 {
@@ -721,18 +721,18 @@ CServerSideClient* FASTCALL Detour_GetFreeClient(int64_t unk1, const __m128i* un
 	return nullptr;
 }
 
-float FASTCALL Detour_CCSPlayerPawn_GetMaxSpeed(CCSPlayerPawn* pPawn)
-{
-	auto flMaxSpeed = CCSPlayerPawn_GetMaxSpeed(pPawn);
-
-	const auto pController = reinterpret_cast<CCSPlayerController*>(pPawn->GetController());
-	if (const auto pPlayer = pController != nullptr ? pController->GetZEPlayer() : nullptr)
-	{
-		flMaxSpeed *= pPlayer->GetMaxSpeed();
-	}
-
-	return flMaxSpeed;
-}
+//float FASTCALL Detour_CCSPlayerPawn_GetMaxSpeed(CCSPlayerPawn* pPawn)
+//{
+//	auto flMaxSpeed = CCSPlayerPawn_GetMaxSpeed(pPawn);
+//
+//	const auto pController = reinterpret_cast<CCSPlayerController*>(pPawn->GetController());
+//	if (const auto pPlayer = pController != nullptr ? pController->GetZEPlayer() : nullptr)
+//	{
+//		flMaxSpeed *= pPlayer->GetMaxSpeed();
+//	}
+//
+//	return flMaxSpeed;
+//}
 
 bool InitDetours(CGameConfig* gameConfig)
 {
